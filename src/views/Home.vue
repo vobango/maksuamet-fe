b0a93369a93fb663d039262f19e620236f51d943
Kaspar Arme <kaspar.arme@reach-u.com>
2021-12-24 14:50:42 +0200

    Test

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Message: {{ message }}</div>
+</template>
+
+<script>
+import superagent from 'superagent';
+
+
+  export default {
+    name: 'Home',
+    data: () => ({
+      message: 'asd'
+    }),
+created: function() {
+  superagent.get('http://localhost:8001/api/test')
+  .withCredentials()
+    .then((res) => {
+      this.message = res.body?.message || 'oh no'
+    })
+}
+  }
+</script>
