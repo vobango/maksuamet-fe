ead2f169b78a3ffad759ecc950ec6a5fa9f94832
Kaspar Arme <kaspar.arme@reach-u.com>
2023-11-18 11:27:41 +0200

    Update proxy conf comment

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ module.exports = {
     'vuetify'
   ],
   devServer: {
-    /* Use localhost for connecting to local back-end or */
+    /* Use localhost for connecting to local back-end or production server */
     proxy: {
       '^/api': {
         target: 'http://localhost:8001',
-        // target: 'https://maksuametnik.ringikoor.ee',
+        // target: 'https://maksuametnik.ringikoor.ee', // NB! Do not make POST requests when using this for testing
         changeOrigin: true
       },
     }
