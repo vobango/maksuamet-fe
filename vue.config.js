5ffe4903177f780369fa0160c91cc416a8c7f2da
Kaspar Arme <kaspar.arme@reach-u.com>
2023-11-18 11:24:42 +0200

    Update Vue config

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,11 @@ module.exports = {
     'vuetify'
   ],
   devServer: {
+    /* Use localhost for connecting to local back-end or */
     proxy: {
       '^/api': {
         target: 'http://localhost:8001',
+        // target: 'https://maksuametnik.ringikoor.ee',
         changeOrigin: true
       },
     }
