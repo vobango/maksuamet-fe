9dba6c4d1191cbb4793f2d961a2d02bc0f7d403d
Kaspar Arme <kaspar.arme@reach-u.com>
2025-09-05 16:35:15 +0300

    Update Nginx users

--- user.sh
+++ user.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 echo "${USER_KOOR}" > /etc/nginx/conf.d/.htpasswd
 echo "${USER_ADMIN}" > /etc/nginx/conf.d/.htpasswd_admin
+echo "${USER_RYHMAVANEM}" >> /etc/nginx/conf.d/.htpasswd_admin
